e745ad5a191350c43857f4a1f75c108a0c046e64
hrithik-sharma25268023 <25268023@studentmail.ul.ie>
2026-06-15 15:07:01 +0100

    image extraction complete

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 """steamlit UI for Table Extraction"""
 
+import os
 import streamlit as st
+from src import pdf_image_utils
 
 def main():
     """main method for app"""
 
     st.title("Table Extractor")
-    st.file_uploader("Upload you PDF:- ")
-    
-    if st.button('Extract'):
-        st.text("Button Pressed")
+
+    file = st.file_uploader("Upload your PDF", type=["pdf"])
+
+    if st.button("Extract") and file:
+        os.makedirs("data/pdf", exist_ok=True)
+
+        with open(f"data/pdf/{file.name}", "wb") as f:
+            f.write(file.getbuffer())
+        os.makedirs(f"data/images/{file.name}", exist_ok=True)
+
+        pdf_image_utils.convert_pdf_to_image(source_path=f"data/pdf/{file.name}",
+                                             destination_path=f"data/images/{file.name}")
+
 
 if __name__ == "__main__":
     main()
---
+++ src/pdf_image_utils.py
@@ -0,0 +1,11 @@
+"""module for utilities for reading PDF and Images."""
+
+from pdf2image import convert_from_path
+
+def convert_pdf_to_image(source_path: str, destination_path: str) -> None:
+    """PDF to Image"""
+
+    images = convert_from_path(source_path)
+    for i in range(len(images)):
+        images[i].save(destination_path+"/"+'page'+ str(i) +'.jpg', 'JPEG')
+        print(f"Page {i} saved.")
