aa35e231d89baf8bc77b9c87599bb96e25f198ae
hrithik-sharma25268023 <25268023@studentmail.ul.ie>
2026-06-16 20:32:44 +0100

    all done

--- app.py
+++ app.py
@@ -2,7 +2,9 @@
 
 import os
 import streamlit as st
-from src import pdf_image_utils
+from tqdm import tqdm
+
+from src import pdf_image_utils, table_extraction
 
 def main():
     """main method for app"""
@@ -17,10 +19,19 @@ def main():
         with open(f"data/pdf/{file.name}", "wb") as f:
             f.write(file.getbuffer())
         os.makedirs(f"data/images/{file.name}", exist_ok=True)
+        os.makedirs(f"data/tables/{file.name}", exist_ok=True)
 
         pdf_image_utils.convert_pdf_to_image(source_path=f"data/pdf/{file.name}",
                                              destination_path=f"data/images/{file.name}")
 
+        for pic in tqdm(os.listdir(f"data/images/{file.name}/")):
+            image, results = table_extraction.table_detection(f"data/images/{file.name}/{pic}")
+            table_extraction.save_results(image, results["scores"],
+                                          results["labels"],
+                                          results["boxes"],
+                                          f"data/tables/{file.name}/{pic.replace('.jpg','')}.jpeg")
+
+        st.success(f"All Tables Extracted.")
 
 if __name__ == "__main__":
     main()
--- src/table_extraction.py
+++ src/table_extraction.py
@@ -1,49 +1,51 @@
-"""Module for table detection and visualization"""
+"""table extraction and detection module"""
 
 from typing import Tuple
+import streamlit as st
 import torch
 from PIL import Image
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from transformers import DetrImageProcessor, TableTransformerForObjectDetection
 
 COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
           [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
 
-MODEL = TableTransformerForObjectDetection.from_pretrained("microsoft/table-transformer-detection")
-PROCESSOR = DetrImageProcessor.from_pretrained("microsoft/table-transformer-detection")
+
+@st.cache_resource
+def load_model() -> None:
+    """model and processor"""
+
+    model = TableTransformerForObjectDetection.from_pretrained("microsoft/table-transformer-detection")
+    processor = DetrImageProcessor.from_pretrained("microsoft/table-transformer-detection")
+    return model, processor
 
 
 def table_detection(image_path: str) -> Tuple:
     """table detection method"""
 
+    model, processor = load_model()
     image = Image.open(image_path).convert("RGB")
-    width, height = image.size
-    image = image.resize((int(width * 0.5), int(height * 0.5)))
-    inputs = PROCESSOR(images=image, return_tensors="pt")
+    inputs = processor(image, return_tensors="pt")
     with torch.no_grad():
-        outputs = MODEL(**inputs)
-    results = PROCESSOR.post_process_object_detection(outputs, threshold=0.7, target_sizes=[(image.size[1], image.size[0])])[0]
+        outputs = model(**inputs)
+    results = processor.post_process_object_detection(outputs, threshold=0.7, target_sizes=[(image.size[1], image.size[0])])[0]
+
     return image, results
 
 
-def save_results(pil_img: Image, scores: torch.Tensor,
-                 labels: torch.Tensor, boxes:torch.Tensor, destination_path: str) -> None:
-    """saves the results of the images"""
+def save_results(pil_img: Image, scores: torch.Tensor, labels: torch.Tensor,
+                 boxes: torch.Tensor, destination_path: str) -> None:
 
-    plt.figure(figsize=(16, 10))
-    plt.imshow(pil_img)
-    ax = plt.gca()
+    model, _ = load_model()
+    fig, ax = plt.subplots(figsize=(16, 10))
+    ax.imshow(pil_img)
     colors = COLORS * 100
     for score, label, box, color in zip(scores.tolist(), labels.tolist(), boxes.tolist(), colors):
         xmin, ymin, xmax, ymax = box
-        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin, fill=False, color=color, linewidth=3))
-        text = f"{MODEL.config.id2label[label]}: {score:.2f}"
-        ax.text(xmin, ymin, text, fontsize=12, bbox=dict(facecolor="yellow", alpha=0.5))
-    plt.axis("off")
-
-
-if __name__ == "__main__":
-    image_path = "/home/hrithik-dev/ds-labs/table-extraction-ci/data/images/American-Express-Annual-Report-2025.pdf/page81.jpg"
-
-    image, results = table_detection(image_path)
-    save_results(image, results["scores"], results["labels"], results["boxes"])
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                     fill=False, color=color, linewidth=3))
+        ax.text(xmin, ymin, f"{model.config.id2label[label]}: {score:.2f}", fontsize=10,
+                bbox=dict(facecolor="yellow", alpha=0.5))
+    ax.axis("off")
+    fig.savefig(destination_path, bbox_inches="tight", pad_inches=0)
+    plt.close(fig)
